a01bc0dd9fb53b29a08bce8a0af94c129363c4c3
Jobirmizo <hishimovanvar@gmail.com>
2024-08-24 16:19:16 +0500

    hello exam OOP

---
+++ Classes/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.AccessControl;
+
+namespace ConsoleApp1.Classes;
+
+public class Subject
+{
+    public int subjectCode { get; set; }
+    public string name { get; set; }
+    public int  totalHours { get; set; }
+    public int credit { get; set; }
+    public int  semester { get; set; }
+
+    public Subject(int code, string name)
+    {
+        this.subjectCode = code;
+        this.name = name;
+    }
+    
+
+    public void show()
+    {
+        Subject subject1 = new Subject(1, "Math");
+        Subject subject2 = new Subject(2, "English Lang");
+        Subject subject3 = new Subject(3, "Cloud Computing");
+    }
+}
---
+++ Classes/Teacher.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1.Classes;
+
+public class Teacher
+{
+    public string fname { get; set; }
+    public string lname { get; set; }
+    public DateTime birthDate { get; set; }
+    public string Departament { get; set; }
+    private List<Subject> subjects;
+
+    public Teacher(string fname, string lname)
+    {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public void teacher()
+    {
+        subjects = new List<Subject>();
+    }
+    public void showSubjects()
+    {
+        
+    }
+
+    static void Main()
+    {
+        Teacher teacher1 = new Teacher("Sardor", "G`aniev");
+        Subject subject1 = new Subject(1, "Math");
+        Subject subject2 = new Subject(2, "English Lang");
+        Subject subject3 = new Subject(3, "Cloud Computing");
+        
+        
+    }
+}
